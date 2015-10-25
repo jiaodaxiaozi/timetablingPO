6fa45f95f211e197bbece1109e8eb332801cb884
Jerker89 <Jerkeran@kth.se>
2015-10-25 15:06:17 +0100

    Added part for solving RMLP using inbuilt matlab linprog solver
    Added parts building matrices for constraints
    Cleaned up notations and comments
    Added a test example to test code
    Added intervals when testing if a variable is zero or one

--- code/BM_Matlab/GeneratePotentialFixings.m
+++ code/BM_Matlab/GeneratePotentialFixings.m
@@ -1,26 +1,26 @@
-%Restricted master LP; RMLP(w,l,u) with costvector w and variables that model variable fixings to 0 and 1, l and u
-%integrality tolerance: epsilon, constant used in potential function: delta,
-%steps whithout progress before spacer step: k_s,maximum number of steps
-%without progress before termination of algoritm: k_max.
+function B_star = GeneratePotentialFixings(l,u,V,A,b,Aeq,beq)
+%test values
 
-function [x_best,B_star] = GeneratePotentialFixings(l,u,x,D,V)
+
 %initial values
-indicator = 0; %indicator if the algoritm should terminate early
 epsilon = 0.2; delta = 0.3; %integrality tolerance: epsilon, constant used in potential function: delta,
 alfa = 0.5; G = max(V(:)); % constant used to update costvector: alfa, bonus weight for spacer step: gamma, 
 i=0; k=0; %Step counters, i is the number of iterations, k is the number of iterations without progress
 k_s = 5; %Number of steps w/o progress before spacer step
 k_max = 10; %maximum number of allowed iterations
-
-
-x_best = x; %x_best is the solution we strongbranch on if it is nessecary
+n_i = true; %True if integer infeasibilities exist 
+int_tol = 10^-6; %Tolerance for intervals around 0 and 1
 B_star = []; %Vector of indexes of potential fixings
 v_star = 0; %Potential function is set to zero initially
 V_perturbed = V; %Initially the revenues of path are unperturbed
-while k < k_max && indicator == 0;
+while k < k_max && n_i == true;
     %solve the Restricted Master LP for the current perbutated cost vector,
     %x_i is the fractional solutions.
-    [x_i] = RMLP(l,u,D,V_perturbed);
+    x_i = RMLP_test(l,u,V_perturbed,A,b,Aeq,beq);
+    
+    if i == 0;
+        x_0 = x_i;
+    end
     %calculate objective value using current x_i and corresponding revenues
     obj = sum(x_i(:).*V(:));
     
@@ -31,26 +31,22 @@ while k < k_max && indicator == 0;
     %progress or not.
     v_x_i = obj + delta*length(B_i);
     
-    %check if all entries in x_i consists of zeroes and ones.
-    %Check index of fractionals that are between 0 and 1
-    index = find(x_i ~= 0 & x_i ~= 1 );
-    counter_non_integer_variables = length(index);
-    %if nr of non-integer entries is zero then all of the variables
-    %that are one are potential fixings.
-    if counter_non_integer_variables == 0;
-        indicator = 1;
+    %check if all entries in x_i are zeroes or ones.
+    index = find((x_i >= int_tol) & (x_i <= 1-int_tol));
+    %If integer solution then potential fixes found
+    if isempty(index) == 1;
+        n_i = false;
         B_star = B_i;
-    end   
-    
+    end
     
     %Spacer step is done if no progress for k_s steps
-    if mod(k,k_s) == 0 && k > 0 && indicator == 0;
+    if mod(k,k_s) == 0 && k > 0 && n_i == true;
         %The index of the largest fractional variable is added to the
         %potential fixings vector.
         
         %Check index of fractionals that are between 0 and 1 not already in
         %B_star or B_i
-        fractional_index = setdiff(index , union(B_star,B_i));
+        fractional_index = setdiff(index, union(B_star,B_i));
         %so long as not all values in x_i is 0 or 1 or potential fixes
         if isempty(fractional_index) == 0; 
             %add all the fractional values to array for fractional values  
@@ -66,18 +62,12 @@ while k < k_max && indicator == 0;
             %Add j_star to the list of potential fixings
             B_star = union(B_i,j_star);
         end
-    %The cost vector is perturbed to hopefully drive fractionals x_i_k
-    %towards integrality   
-    %when the potential function v_x_i increase we make progress and
-    %update our set of potential fixings.
     else
         if v_x_i > v_star;
             %Update the vector for potential fixings, potential function
             %and since we made progress we reduce the number of steps w/o
             %progress.
             B_star = B_i; v_star = v_x_i; k = k - 1;
-            %remember current solution
-            x_best = x_i;
         end
         %the perturbation  made to all elements in income matrix
         V_perturbed = alfa*V_perturbed.*(x_i).^2 + V_perturbed;
@@ -87,9 +77,9 @@ while k < k_max && indicator == 0;
 end
 
 %If B_star is empty then choose a potential variable to fix by
-%strongbranching. May also try heuristically to fix the highest fraction
+%strongbranching.
 if isempty(B_star) == 1;
-    j_star = strongbranching(x_best,l,u,D,V);
+    j_star = strongbranching(x_0,l,u,V,A,b,Aeq,beq);
     B_star = j_star;
 end
 end
--- code/BM_Matlab/strongbranching.m
+++ code/BM_Matlab/strongbranching.m
@@ -1,18 +1,9 @@
-%If x_i_k = a for some element of x_i then strong branching introduces the
-%constraint x_i_k < a. this prouduces a new subproblem that is solved. Then
-%the same is done but with the constrain x_i_k > a. This is repeated for
-%all fractionals or a subset of the fractionals in x_i. The constraint that
-%gives the best bound is then chosen. The way to choose subset to test may vary.
-%Here I choose the r largest fractions. Might only want to find a
-%variable to set to 1. In that case then we only look at the optimal
-%objective value for x_i_k > a.
-
-
-function j_star = strongbranching(x_best,l,u,D,V)
+function j_star = strongbranching(x_0,l,u,V,A,b,Aeq,beq)
+int_tol = 10^-6; %Tolerance for intervals around 0 and 1
 V_perturbated = V;
-r = 10;%Maximum number of fractions examined
+r = 5;%Maximum number of fractions examined
 %Check index of fractionals that are between 0 and 1
-index = find(x_best ~= 0 & x_best ~= 1 );
+index = find((x_0 >= int_tol) & (x_0 <= 1-int_tol) & (l ~= 1) & (u ~= 0));
 %the size of the branching index vector is the minimum of r and the nr of
 %fractions
 nr_of_branches = min(r,length(index));
@@ -20,14 +11,14 @@ nr_of_branches = min(r,length(index));
 if isempty(index) == 0;    
     %add all the fractional values to array for fractional
     %values
-    fractionals = x_best(index);
+    fractionals = x_0(index);
 end
 %Array for objective values.
 Obj = zeros(nr_of_branches,1);
-%Array for possible best indexes
+%Array for possible best indexes to fix
 best_index = zeros(nr_of_branches,1);
 for t = 1:nr_of_branches;
-    %temporary bound, last letter says we let x_i_k > a
+    %temporary bound
     l_t_u = l;
     %find the index of the maximum fractional in fractionals and the maximum fraction.
     [~, max_fractional_index_in_fractionals] = max(fractionals);
@@ -36,11 +27,11 @@ for t = 1:nr_of_branches;
     %Remember this index
     best_index(t) = max_fractional_index_in_fractionals;
     %find max fractional index in x_best
-    x_best_max_index = index(max_fractional_index_in_fractionals);
+    x_0_max_index = index(max_fractional_index_in_fractionals);
     %Change temporary lower bound for variable with maximum fraction.
-    l_t_u(x_best_max_index) = 1;
-    %solve RLMP(w,l_t_u,u)
-    [x_i] = RMLP(l_t_u,u,D,V_perturbated);
+    l_t_u(x_0_max_index) = 1;
+    %solve RLMP
+    [x_i] = RMLP_test(l_t_u,u,V_perturbated,A,b,Aeq,beq);
     %calculate objective value using current x_i and corresponding revenues
     obj_u = sum(x_i(:).*V(:));
     %add the objective value to the objective value vector
@@ -49,7 +40,7 @@ for t = 1:nr_of_branches;
     
 end
 %find the maximal  objective value in Obj
-[max_value, max_index] = max(Obj);
-j_star = Best_index(max_index);
+[~, max_index] = max(Obj);
+j_star = index(best_index(max_index));
 
 end
